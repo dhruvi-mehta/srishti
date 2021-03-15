e098b46a5aa6c92d3bfcb10abd5350e0e35b5724
dhruvi-mehta <mehtd083@newschool.edu>
2021-03-15 00:47:32 -0400

    hi

---
+++ script.js
@@ -0,0 +1,184 @@
+document.body.style.backgroundColor = "#ffecd9"
+
+
+function clickOne(){
+	one.classList.add("clear")
+	two.classList.remove("clear")
+	three.classList.remove("clear")
+	four.classList.remove("clear")
+	five.classList.remove("clear")
+	six.classList.remove("clear")
+	seven.classList.remove("clear")
+	eight.classList.remove("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = "Its been a long time coming !! we just can't believe that you're leaving, and just can't imagine home without you :( "
+}
+
+let one = document.querySelector("#one");
+one.addEventListener("click", clickOne);
+
+
+
+
+
+function clickTwo(){
+	one.classList.remove("clear")
+	two.classList.add("clear")
+	three.classList.remove("clear")
+	four.classList.remove("clear")
+	five.classList.remove("clear")
+	six.classList.remove("clear")
+	seven.classList.remove("clear")
+	eight.classList.remove("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = " we cannot thank you enough for the countless times you have been there for us. thank you for never leaving our side and having our back even in the most difficult times ever."
+}
+
+let two = document.querySelector("#two");
+two.addEventListener("click", clickTwo);
+
+
+
+function clickThree(){
+	one.classList.remove("clear")
+	two.classList.remove("clear")
+	three.classList.add("clear")
+	four.classList.remove("clear")
+	five.classList.remove("clear")
+	six.classList.remove("clear")
+	seven.classList.remove("clear")
+	eight.classList.remove("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = "thank you for being the example of a true and supportive friend, stayed by us while we cried, and while we celebrated "
+}
+
+let three = document.querySelector("#three");
+three.addEventListener("click", clickThree);
+
+
+function clickFour(){
+	one.classList.remove("clear")
+	two.classList.remove("clear")
+	three.classList.remove("clear")
+	four.classList.add("clear")
+	five.classList.remove("clear")
+	six.classList.remove("clear")
+	seven.classList.remove("clear")
+	eight.classList.remove("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = "But we just wanted you to know !! that we are always going to be there for you no matter what !!"
+}
+
+let four = document.querySelector("#four");
+four.addEventListener("click", clickFour);
+
+
+
+function clickFive(){
+	one.classList.remove("clear")
+	two.classList.remove("clear")
+	three.classList.remove("clear")
+	four.classList.remove("clear")
+	five.classList.add("clear")
+	six.classList.remove("clear")
+	seven.classList.remove("clear")
+	eight.classList.remove("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = "HELLO ALSO YOU ARE MOVING TO BROOKLYN SO SO SO SO SO SO SO SO SOON"
+}
+
+let five = document.querySelector("#five");
+five.addEventListener("click", clickFive);
+
+function clickSix(){
+	one.classList.remove("clear")
+	two.classList.remove("clear")
+	three.classList.remove("clear")
+	four.classList.remove("clear")
+	five.classList.remove("clear")
+	six.classList.add("clear")
+	seven.classList.remove("clear")
+	eight.classList.remove("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = " GET READY FOR SO MANY MEET UPS in NYC!!"
+}
+
+let six = document.querySelector("#six");
+six.addEventListener("click", clickSix);
+
+
+function clickSeven(){
+	one.classList.remove("clear")
+	two.classList.remove("clear")
+	three.classList.remove("clear")
+	four.classList.remove("clear")
+	five.classList.remove("clear")
+	six.classList.remove("clear")
+	seven.classList.add("clear")
+	eight.classList.remove("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = "we are so so so so grateful to have you as a friend and genuinely don't know what we would do without you !!"
+}
+
+let seven = document.querySelector("#seven");
+seven.addEventListener("click", clickSeven);
+
+
+function clickEight(){
+	one.classList.remove("clear")
+	two.classList.remove("clear")
+	three.classList.remove("clear")
+	four.classList.remove("clear")
+	five.classList.remove("clear")
+	six.classList.remove("clear")
+	seven.classList.remove("clear")
+	eight.classList.add("clear")
+	nine.classList.remove("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = "love you to the moon and back^10000000"
+}
+
+let eight = document.querySelector("#eight");
+eight.addEventListener("click", clickEight);
+
+
+function clickNine(){
+	one.classList.remove("clear")
+	two.classList.remove("clear")
+	three.classList.remove("clear")
+	four.classList.remove("clear")
+	five.classList.remove("clear")
+	six.classList.remove("clear")
+	seven.classList.remove("clear")
+	eight.classList.remove("clear")
+	nine.classList.add("clear")
+	let p = document.querySelector("#bio");
+	p.innerHTML = "also ps say hi to your mum (hi aunty) and give her a hug from us <3!"
+}
+
+let nine = document.querySelector("#nine");
+nine.addEventListener("click", clickNine);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
